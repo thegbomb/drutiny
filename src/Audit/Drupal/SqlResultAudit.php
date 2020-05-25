f60535c519494375901a52750e0b072a7d7c156d
Rohit Joshi <joshi.rohit100@gmail.com>
2020-05-25 11:27:18 +0530

    Allow SqlResultAudit to allow non select queries as well.

--- src/Audit/Drupal/SqlResultAudit.php
+++ src/Audit/Drupal/SqlResultAudit.php
@@ -7,6 +7,26 @@ use Drutiny\Sandbox\Sandbox;
 
 /**
  * Audit the first row returned from a SQL query.
+<<<<<<< HEAD
+=======
+ * @Param(
+ *  name = "query",
+ *  description = "The SQL query to run. Can use other parameters for variable replacement.",
+ *  type = "string"
+ * )
+ * @param(
+ *  name = "db_level_query",
+ *  description = "Whether query is normal sql query like 'SELECT * FROM *' or a DB level query like 'SHOW TABLE STATUS'",
+ *  type = "boolean",
+ *  default = false
+ * )
+ * @Param(
+ *  name = "expression",
+ *  description = "An expression language expression to evaluate a successful auditable outcome.",
+ *  type = "string",
+ *  default = true
+ * )
+>>>>>>> 798c052... Allow SqlResultAudit to allow non select queries as well.
  * @Token(
  *  name = "result",
  *  description = "The comparison operator to use for the comparison.",
@@ -18,8 +38,20 @@ use Drutiny\Sandbox\Sandbox;
  *  type = "string"
  * )
  */
+<<<<<<< HEAD
 class SqlResultAudit extends AbstractAnalysis
 {
+=======
+class SqlResultAudit extends AbstractAnalysis {
+
+  /**
+   *
+   */
+  public function gather(Sandbox $sandbox)
+  {
+    $query = $sandbox->getParameter('query');
+    $db_level_query = $sandbox->getParameter('db_level_query');
+>>>>>>> 798c052... Allow SqlResultAudit to allow non select queries as well.
 
     /**
      * {@inheritdoc}
@@ -34,12 +66,29 @@ class SqlResultAudit extends AbstractAnalysis
         parent::configure();
     }
 
+<<<<<<< HEAD
     /**
      * {@inheritdoc}
      */
     public function gather(Sandbox $sandbox)
     {
         $query = $this->getParameter('query');
+=======
+    if (!$db_level_query) {
+      if (!preg_match_all('/^SELECT( DISTINCT)? (.*) FROM/', $query, $fields)) {
+        throw new \Exception("Could not parse fields from SQL query: $query.");
+      }
+      $fields = array_map('trim', explode(',', $fields[2][0]));
+      foreach ($fields as &$field) {
+        if ($idx = strpos($field, ' as ')) {
+          $field = substr($field, $idx + 4);
+        }
+        elseif (preg_match('/[ \(\)]/', $field)) {
+          throw new \Exception("SQL query contains an non-table field without an alias: '$field.'");
+        }
+      }
+    }
+>>>>>>> 798c052... Allow SqlResultAudit to allow non select queries as well.
 
         // Migrate 2.x queries to 3.x
         $query = strtr($query, [
@@ -60,6 +109,7 @@ class SqlResultAudit extends AbstractAnalysis
               return array_filter($data);
           });
 
+<<<<<<< HEAD
         $fields = $this->getFieldsFromSql($query);
         if (!empty($fields)) {
             $result = array_map(function ($row) use ($fields) {
@@ -70,6 +120,13 @@ class SqlResultAudit extends AbstractAnalysis
         $this->set('count', count($result));
         $this->set('results', $result);
         $this->set('first_row', array_shift($result));
+=======
+    while ($line = array_shift($output))
+    {
+      $values = array_map('trim', explode("\t", $line));
+      $results[] = array_combine($fields, $values);
+      $results[] = !$db_level_query ? array_combine($fields, $values) : $values;
+>>>>>>> 798c052... Allow SqlResultAudit to allow non select queries as well.
     }
 
     protected function getFieldsFromSql($query):array
