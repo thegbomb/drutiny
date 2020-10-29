acfa390bab388adff9c3c521de5b62782d0b2ef9
Brian Schilt <brian.schilt@gmail.com>
2020-10-29 12:42:03 -0700

    Theme adjustments (#260)
    * Added the table-hover class to policy tables to get row highlightng.
    * Update policy HTML templates to use the card markup.
    * Fixed bug where a blank HTML tag was being output.
    * Added styling for .assessment-uri class.
    * Extended policy container width to fill parent.
    * Adding table-hover class.
    * Added top and bottom padding to the HTML policy output.
    * Adjusting policy padding.
    * Adjusting alert box background colors.

--- src/Report/Format/MarkdownHelper.php
+++ src/Report/Format/MarkdownHelper.php
@@ -20,8 +20,11 @@ class MarkdownHelper extends Parsedown
 
     protected function blockTable($Line, array $Block = null)
     {
-        $block = parent::blockTable($Line, $Block);
-        $block['element']['attributes']['class'] = 'table';
-        return $block;
+      $block = parent::blockTable($Line, $Block);
+      if (isset($block['element']['name'])) {
+        $block['element']['attributes']['class'] = 'table table-hover';
+      }
+
+      return $block;
     }
 }
