6eeea9ba29c2476c3a1f9209cae290e308c48d03
Josh Waihi <josh.waihi@acquia.com>
2021-04-07 20:19:46 +1200

    New boot messages.

--- drutiny.php
+++ drutiny.php
@@ -1,7 +1,9 @@
 <?php
 
 $messages = [
-  'Drutiny is preparing of super position of quantum uncertainty... please wait 🧪',
-  'Drutiny is reversing polarity on the Tardus... please wait'
+  'Drutiny is preparing a super position of quantum uncertainty ... please wait 🧪',
+  'Drutiny is reversing polarity with a sonic screwdriver ... please wait',
+  'Drutiny is co-ordinating with Ziggy for a Quantum Leap ... please wait ⚛️',
+  'Drutiny is hailing the USS Discovery ... Black Alert 🌌',
 ];
 $container->setParameter('progress_bar.loading_message', $messages[array_rand($messages)]);
