8e9727fb570ad67d3bc3356db6ba1f2dec3fc825
Josh Waihi <josh.waihi@acquia.com>
2021-11-30 14:56:54 +1300

    Rick and Morty reference.

--- drutiny.php
+++ drutiny.php
@@ -5,5 +5,6 @@ $messages = [
   'Drutiny is reversing polarity with a sonic screwdriver ... please wait',
   'Drutiny is co-ordinating with Ziggy for a Quantum Leap ... please wait ⚛️',
   'Drutiny is hailing the USS Discovery ... Black Alert 🌌',
+  'Wubba Lubba Dub Dub! 🎉',
 ];
 $container->setParameter('progress_bar.loading_message', $messages[array_rand($messages)]);
