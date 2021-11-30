c40221540828a470190673213804c9466a3f91be
Josh Waihi <josh.waihi@acquia.com>
2021-11-30 14:58:56 +1300

    DDEV support! ....and docker.

---
+++ src/Target/DdevTarget.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drutiny\Target;
+
+use Drutiny\Target\Service\DrushService;
+use Drutiny\Target\Service\DockerService;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+/**
+ * Target for parsing Drush aliases.
+ */
+class DdevTarget extends DrushTarget implements TargetInterface, TargetSourceInterface
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function getId():string
+  {
+    return $this['ddev.name'];
+  }
+
+  /**
+   * @inheritdoc
+   * Implements Target::parse().
+   */
+    public function parse($alias):TargetInterface
+    {
+
+        $this['ddev.name'] = $alias;
+
+        $status_cmd = sprintf('ddev describe %s -j', $alias);
+        $ddev = $this['service.local']->run($status_cmd, function ($output) {
+          $json = json_decode($output, true);
+          return $json['raw'];
+        });
+
+        if (empty($ddev)) {
+          throw new InvalidTargetException("DDEV site '$alias' either doesn't exist or is not currently active.");
+        }
+
+        $this['ddev'] = $ddev;
+        $this['service.docker'] = new DockerService($this['service.local']);
+        $this['service.docker']->setContainer($ddev['services']['web']['full_name']);
+        $this['service.exec'] = $this['service.docker'];
+
+        $this['drush.root'] = '/var/www/html';
+
+        // Provide a default URI if none already provided.
+        $this->setUri($ddev['primary_url']);
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAvailableTargets():array
+    {
+      $aliases = $this['service.local']->run('ddev list -A -j', function ($output) {
+        $json = json_decode($output, true);
+        return array_combine(array_column($json['raw'], 'name'), $json['raw']);
+      });
+
+      $targets = [];
+      foreach ($aliases as $name => $info) {
+        $targets[] = [
+          'id' => $name,
+          'uri' => $info['primary_url'] ?? '',
+          'name' => $name
+        ];
+      }
+      return $targets;
+    }
+}
---
+++ src/Target/Service/DockerService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drutiny\Target\Service;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class DockerService implements ExecutionInterface {
+  protected $local;
+  protected $container;
+
+  public function __construct(LocalService $local)
+  {
+    $this->local = $local;
+  }
+
+  public function setContainer(string $container_name):ExecutionInterface
+  {
+    $this->container = $container_name;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run(string $cmd, callable $preProcess = NULL, int $ttl = 3600)
+  {
+    $cmd = $this->local->replacePlaceholders($cmd);
+    $cmd = sprintf("docker exec -t %s sh -c 'echo %s | base64 --decode | sh'", $this->container, base64_encode($cmd));
+
+    return $this->local->run($cmd, $preProcess, $ttl);
+  }
+}
