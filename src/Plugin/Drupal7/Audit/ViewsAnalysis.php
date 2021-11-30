633cc1e9b1da7ad84ed78be5af63d838e450d5fd
Josh Waihi <josh.waihi@acquia.com>
2021-11-30 15:02:19 +1300

    Audit class for Drupal 7 views analysis.

---
+++ src/Plugin/Drupal7/Audit/ViewsAnalysis.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drutiny\Plugin\Drupal7\Audit;
+
+use Drutiny\Audit\AbstractAnalysis;
+use Drutiny\Sandbox\Sandbox;
+
+/**
+ * Views Analysis
+ */
+class ViewsAnalysis extends AbstractAnalysis {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function gather(Sandbox $sandbox) {
+    // Gather limited views data since views recurse and can result in a
+    // lot of data being transfered.
+    $views = $this->target->getService('drush')->runtime(function () {
+      $data = [];
+      foreach (views_get_all_views() as $view) {
+        $data[$view->name] = [
+          'name' => $view->name,
+          'human_name' => $view->human_name,
+          'base_table' => $view->base_table,
+          'export_module' => $view->export_module,
+          'display' => array_map(fn ($d) => $d->display_title, $view->display),
+        ];
+      }
+      return $data;
+    });
+
+    $this->set('views', $views);
+  }
+}
