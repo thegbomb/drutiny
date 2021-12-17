b51f2f410d1df468376542ef11bb72fee8c304a6
Omkar Pednekar <omkar.pednekar@acquia.com>
2021-12-17 15:58:45 +0530

    ISSUE-305: Adding audit class for D7 sites.

---
+++ src/Plugin/Drupal7/Audit/Database/DatabaseConnectionAnalysis.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drutiny\Plugin\Drupal7\Audit;
+
+use Drutiny\Audit\AbstractAnalysis;
+use Drutiny\Sandbox\Sandbox;
+
+/**
+ * Provides all database connection information.
+ */
+class DatabaseConnectionAnalysis extends AbstractAnalysis {
+
+  public function gather(Sandbox $sandbox) {
+    $db_connections = $this->target->getService('drush')
+      ->runtime(function() {
+        global $databases;
+        return $databases;
+    });
+    $this->unset_user_pass($db_connections, ['username', 'password']);
+    $this->set('database_connections', $db_connections);
+  }
+
+  /**
+   * Helper function to unset non-required keys from connections array.
+   * @param array $connections
+   * @param array $unwanted_keys
+   */
+  public function unset_user_pass(array &$connections, array $unwanted_keys) {
+    foreach ($unwanted_keys as $unwanted_key) {
+      unset($connections[$unwanted_key]);
+    }
+    foreach ($connections as &$connection) {
+      if (is_array($connection)) {
+          $this->unset_user_pass($connection, $unwanted_keys);
+      }
+    }
+  }
+
+}
