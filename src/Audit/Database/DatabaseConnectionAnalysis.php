ebe640d1bc698c39778e67b914a9d892eefb57e2
Omkar Pednekar <omkar.pednekar@acquia.com>
2021-12-17 15:11:10 +0530

    ISSUE-305: Adding new class to fetch database configurations.

---
+++ src/Audit/Database/DatabaseConnectionAnalysis.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drutiny\Audit\Database;
+
+use Drutiny\Audit\AbstractAnalysis;
+use Drutiny\Sandbox\Sandbox;
+
+/**
+ * Provides all database connection information.
+ */
+class DatabaseConnectionAnalysis extends AbstractAnalysis {
+
+  public function gather(Sandbox $sandbox) {
+    $db_connections = $this->target->getService('drush')
+      ->runtime(function() {
+        return \Drupal\Core\Database\Database::getAllConnectionInfo();
+    });
+    $this->unset_user_pass($db_connections, ['username', 'password']);
+    $this->set('database_connections', $db_connections);
+  }
+
+  /**
+   * Helper function to unset non-required keys from connections array.
+   * @param array $connections
+   * @param array $unwanted_keys
+   */
+  public function unset_user_pass(array &$connections, array $unwanted_keys) {
+    foreach ($unwanted_keys as $unwanted_key) {
+      unset($connections[$unwanted_key]);
+    }
+    foreach ($connections as &$connection) {
+      if (is_array($connection)) {
+          $this->unset_user_pass($connection, $unwanted_keys);
+      }
+    }
+  }
+
+}
