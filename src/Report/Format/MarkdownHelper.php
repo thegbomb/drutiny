39c86730d20791cd8d9f496100c6057cddf77c12
Josh Waihi <josh.waihi@acquia.com>
2021-12-24 08:37:50 +1300

    Small fix for php notices in markdown helper for bootstrap.

--- src/Report/Format/MarkdownHelper.php
+++ src/Report/Format/MarkdownHelper.php
@@ -22,14 +22,14 @@ class MarkdownHelper extends Parsedown
     {
       $block = parent::blockTable($Line, $Block);
       if (isset($block['element']['name'])) {
-        // Add the class attribute to the <table> tag. 
+        // Add the class attribute to the <table> tag.
         $block['element']['attributes']['class'] = 'table table-hover';
 
         // Add the table-active class to the thead > tr element.
         $block['element']['text'][0]['text'][0]['attributes']['class'] = 'table-active';
 
         // Add the scope='col' attribute to the th elements.
-        $thead_columns = $block['element']['text'][0]['text'][0]['text'];
+        $thead_columns = $block['element']['text'][0]['text'][0]['text'] ?? [];
         foreach ($thead_columns as $i => $col) {
           $block['element']['text'][0]['text'][0]['text'][$i]['attributes']['scope'] = 'col';
         }
