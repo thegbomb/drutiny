268e237bf124fbd6e8ff89ff76880b93bb357785
Josh Waihi <josh.waihi@acquia.com>
2022-01-18 11:33:07 +1300

    Move most of the CLI logic into a stub file for easier phar building.

--- stub.php
+++ stub.php
@@ -1,31 +1,43 @@
 <?php
 
-/**
- * @file
- * Stub file for the phar build.
- */
+use Drutiny\Kernel;
+use Drutiny\Console\Application;
 
- use Drutiny\Kernel;
- use Drutiny\Console\Application;
+if (!in_array(PHP_SAPI, ['cli', 'phpdbg', 'embed'], true)) {
+    echo 'Warning: The console should be invoked via the CLI version of PHP, not the '.PHP_SAPI.' SAPI'.PHP_EOL;
+}
 
- if (!in_array(PHP_SAPI, ['cli', 'phpdbg', 'embed'], true)) {
-     echo 'Warning: The console should be invoked via the CLI version of PHP, not the '.PHP_SAPI.' SAPI'.PHP_EOL;
- }
+list($major, $minor, ) = explode('.', phpversion());
+
+// Identify PHP versions lower than PHP 7.4.
+if ($major < 7 || ($major > 7 && $minor < 4)) {
+  echo "ERROR: Application requires PHP 7.4 or later. Currently running: ".phpversion()."\n";
+  exit;
+}
 
 set_time_limit(0);
-ini_set('memory_limit', '-1');
 
-require 'vendor/autoload.php';
+$lib = Phar::running() ?: '.';
+define('DRUTINY_LIB', $lib);
 
-const DRUTINY_LIB = __DIR__;
+require DRUTINY_LIB.'/vendor/autoload.php';
 
-$version_filename = DRUTINY_LIB . '/VERSION';
-$version = 'unknown';
-if (file_exists($version_filename)) {
- $version = file_get_contents($version_filename);
+$version_files = [DRUTINY_LIB.'/VERSION', dirname(__DIR__).'/VERSION'];
+
+// Load in the version if it can be found.
+$versions = array_filter(array_map(function($file) {
+  return file_exists($file) ? file_get_contents($file) : FALSE;
+}, $version_files));
+
+// Load from git.
+if (empty($versions) && !Phar::running() && file_exists(DRUTINY_LIB . '/.git') && $git_bin = exec('which git')) {
+  $versions[] = exec(sprintf('%s -C %s branch --no-color | cut -b 3-', $git_bin, DRUTINY_LIB)) . '-dev';
 }
+
+// Fallback option.
+$versions[] = 'dev';
 
 $kernel = new Kernel('production');
 
-$application = new Application($kernel, $version);
+$application = new Application($kernel, reset($versions));
 $application->run();
