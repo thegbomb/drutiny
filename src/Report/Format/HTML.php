5480ef9927dcde8d64cc31500e476c785323c5ff
Josh Waihi <josh.waihi@acquia.com>
2022-04-13 06:45:52 +1200

    Removed support for 2.x format of profiles in HTML reporting.

--- src/Report/Format/HTML.php
+++ src/Report/Format/HTML.php
@@ -28,44 +28,19 @@ class HTML extends TwigFormat
 
     protected function prepareContent(Profile $profile, AssessmentInterface $assessment):array
     {
+      // 2.x style no longer supported.
+      if (is_array($this->options['content'])) {
+        throw new \Exception("format.html.content in profile is using 2.x format. Not supported in 3.4 or later.");
+      }
+
       $variables = ['profile' => $profile, 'assessment' => $assessment];
       $sections = [];
       $markdown = $this->twig->getRuntime('Twig\Extra\Markdown\MarkdownRuntime');
 
       // In 3.x we support Twig TemplateWrappers to be passed directly
       // to the report format.
-      if ($this->options['content'] instanceof TemplateWrapper) {
-        foreach ($this->options['content']->getBlockNames() as $block){
-          $sections[] = $markdown->convert($this->options['content']->renderBlock($block, $variables));
-        }
-        return $sections;
-      }
-
-      // Backward compatible 2.x Yaml style.
-      foreach ($this->options['content'] as $section) {
-        foreach ($section as $attribute => $value) {
-          $template = $this->prefixTemplate($section[$attribute]);
-
-          try {
-            // Convert template into a Twig template and render into HTML.
-            $this->logger->debug("Creating section twig template for '$attribute' from string.");
-            $template = $this->twig
-              ->createTemplate($template)
-              ->render($variables);
-          }
-          catch (\Twig\Error\Error $e) {
-            $this->logger->error($e->getMessage());
-            $source = $e->getSourceContext();
-            $this->logger->info($source->getCode());
-            $this->logger->debug("Original template: \n".$section[$attribute]);
-          }
-
-          // Convert any Markdown formatting over to HTML.
-          $template = ($attribute == 'body') ? $markdown->convert($template) : $template;
-
-          $section[$attribute] = $template;
-        }
-        $sections[] = $section;
+      foreach ($this->options['content']->getBlockNames() as $block){
+        $sections[] = $markdown->convert($this->options['content']->renderBlock($block, $variables));
       }
       return $sections;
     }
